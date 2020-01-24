94c4cc81f846eb99e6711e12908603e4d1c85db9
Umeh Solomon <50748164+soloking2@users.noreply.github.com>
2020-01-24 10:04:11 +0100

    committed

--- src/app/staff/staff-list.component.ts
+++ src/app/staff/staff-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StaffService } from '../staff.service';
 
 @Component({
   selector: 'app-staff-list',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private staffService: StaffService) { }
 
   ngOnInit() {
+    
   }
 
 }
